937fcb5e90e7f244d3908521b17e7f3ea1e5a04f
Ehsan Maiqani <spin313@gmail.com>
2023-03-05 10:26:01 +0330

    Reverse text used

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import md2html
+from reverse_text import ReverseText
 
 md2html.MarkdownToHtml(input_filename="README.md", output_filename="HTML.html").convert()
+
+text = input('Enter the text here: ')
+
+reverseText = ReverseText(text)
+
+print(reverseText.reverse_character_wise())
+print(reverseText.reverse_character_wise_o1())
+print(reverseText.reverse_word_wise())

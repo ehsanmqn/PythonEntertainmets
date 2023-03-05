70195b8ed08a936843c3b05de90c9c646a4ce26d
Ehsan <spin313@gmail.com>
2023-03-05 21:51:38 +0330

    Unittests added for MarkdownToHtml

--- tests.py
+++ tests.py
@@ -40,6 +40,46 @@ class MarkdownToHtmlTest(unittest.TestCase):
     def setUp(cls) -> None:
         print("MarkdownToHtmlTest: Run for every test")
 
+    def test_convert_text_h1(self):
+        input_markdown = "# h1 Heading 8-)"
+        output_html = "<h1>h1 Heading 8-)</h1>\n"
+
+        processed_text = MarkdownToHtml(input_text=input_markdown).convert_text()
+
+        self.assertEqual(processed_text, output_html)
+
+    def test_convert_text_h2(self):
+        input_markdown = "## h2 Heading 8-)"
+        output_html = "<h2>h2 Heading 8-)</h2>\n"
+
+        processed_text = MarkdownToHtml(input_text=input_markdown).convert_text()
+
+        self.assertEqual(processed_text, output_html)
+
+    def test_convert_text_h3(self):
+        input_markdown = "### h3 Heading 8-)"
+        output_html = "<h3>h3 Heading 8-)</h3>\n"
+
+        processed_text = MarkdownToHtml(input_text=input_markdown).convert_text()
+
+        self.assertEqual(processed_text, output_html)
+
+    def test_convert_text_bold(self):
+        input_markdown = "**bolded** not bolded"
+        output_html = "<p>\n<b>bolded</b> not bolded</p>\n"
+
+        processed_text = MarkdownToHtml(input_text=input_markdown).convert_text()
+
+        self.assertEqual(processed_text, output_html)
+
+    def test_convert_text_p(self):
+        input_markdown = "bolded not bolded"
+        output_html = "<p>\nbolded not bolded</p>\n"
+
+        processed_text = MarkdownToHtml(input_text=input_markdown).convert_text()
+
+        self.assertEqual(processed_text, output_html)
+
 
 if __name__ == '__main__':
     unittest.main()

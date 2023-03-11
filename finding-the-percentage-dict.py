1eee5bd8e95c79c567442bf27842e33978f23ef3
Ehsan Maiqani <spin313@gmail.com>
2023-03-11 15:51:01 +0330

    hash function added html parser added finding percentage added

---
+++ finding-the-percentage-dict.py
@@ -0,0 +1,17 @@
+import math
+
+
+def average(data):
+    return sum(data) / len(data)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    print("{:.2f}".format(average(student_marks[query_name])))
---
+++ html_parser.py
@@ -0,0 +1,57 @@
+
+
+# create a subclass and override the handler methods
+class MyHTMLParser():
+
+    def feed(self, data):
+        self.examine_data(data, True)
+
+    def examine_data(self, data, is_start):
+        print(is_start)
+        if len(data) == 0:
+            return
+
+        if data[0] == "<":
+            tag = ""
+            data.pop(0)
+            character = data.pop(0)
+
+            while character != ">":
+                tag += character
+                character = data.pop(0)
+
+            if "/" in tag:
+                tag = tag.replace("/", "")
+
+                if is_start is False:
+
+                self.handle_endtag(tag=tag)
+            else:
+                self.handle_starttag(tag=tag)
+
+            return self.examine_data(data, not is_start)
+
+        while True:
+            character = data[0]
+            if character != "<":
+                data.pop(0)
+            else:
+                break
+
+        self.examine_data(data, not is_start)
+
+    def handle_starttag(self, tag, attrs=None):
+        print("Start  :", tag)
+
+    def handle_endtag(self, tag):
+        print("End   :", tag)
+
+    def handle_startendtag(self, tag, attrs=None):
+        print(" :", tag)
+
+
+# instantiate the parser and fed it some HTML
+parser = MyHTMLParser()
+data = "<html><head><title>HTML Parser - I</title></head>" + "<body><h1>HackerRank</h1><br /></body></html>"
+parser.feed(list(data))
+

683dacb87036cb68df30f1754aa1980823aedb00
Ehsan Maiqani <spin313@gmail.com>
2023-03-12 11:16:08 +0330

    number convertor updated

--- number-convertor.py
+++ number-convertor.py
@@ -1,12 +1,19 @@
 def print_formatted(input_num):
     for number in range(1, input_num + 1):
-        binary = bin(number)
-        decimal = "".join([' '] * (len(str(binary)) - len(str(number)))) + str(number)
-        octal = "".join([' '] * (len(str(binary)) - len(str(oct(number))))) + str(oct(number))
-        hexadecimal = "".join([' '] * (len(str(binary)) - len(str(hex(number))))) + str(hex(number))
-        binary = "".join([' '] * (len(str(binary)) - len(str(bin(number))))) + str(bin(number))
+        padding = len(str(bin(input_num))[2:])
 
-        print("{}{}{}{}".format(decimal, octal, hexadecimal, binary))
+        binary = bin(number)
+        binary_str = str(binary)[2:]
+        octal_str = str(oct(number))[2:]
+        hexadecimal_str = (str(hex(number))[2:]).upper()
+
+        decimal = "".join([' '] * (padding - len(str(number)))) + str(number)
+        octal = "".join([' '] * (padding - len(str(octal_str)))) + str(octal_str)
+        hexadecimal = "".join([' '] * (padding - len(str(hexadecimal_str)))) + str(hexadecimal_str)
+        binary = "".join([' '] * (padding - len(str(binary_str)))) + str(binary_str)
+
+        print("{} {} {} {}".format(decimal, octal, hexadecimal, binary))
+
 
 if __name__ == '__main__':
     n = int(input())
